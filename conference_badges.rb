0644e4a8b333eeac1f810c53f27eb7244bc1cce9
Teanna <teannamtaylor95@gmail.com>
2023-05-13 09:03:45 -0700

    commit tree ab08d5651aba0acb123af2d9176ba768551e1500

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    array.map{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) do |person, index|
+    "Hello, #{person}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(attendees)
+    badge_messages = batch_badge_creator(attendees)
+    badge_messages.map{|message| puts message}
+
+    room_assignments = assign_rooms(attendees)
+    room_assignments.map {|assignment| puts assignment}
+
+end
